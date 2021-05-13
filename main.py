eef7cd13b8bb471552bc4e3e5d1f66353d28f296
NOH HYEONSEONG <shgustjd6262@naver.com>
2021-05-13 20:02:39 +0900

    주석

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ import poketmon
 import budget
 
 if __name__ == '__main__':
+    
+    #프로그래머스 레벨1 포켓몬 문제
     #poketmon_nums = [3,3,3,2,2,4]
     #poketmon.solution(poketmon_nums)
 
-    budget_d = [1,3,2,5,4]
-    budget_budget = 9
-    budget.solution(budget_d,budget_budget)
+    # 프로그래머스 레벨1 예산 문제
+    #budget_d = [1,3,2,5,4]
+    #budget_budget = 9
+    #budget.solution(budget_d,budget_budget)
